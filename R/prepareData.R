8cd09f0516b352137fd839c6c32ead2d7ecc90f6
Jesper Madsen <jespergrud@gmail.com>
2022-11-14 14:28:30 +0100

    Update prepareData.R

--- R/prepareData.R
+++ R/prepareData.R
@@ -58,7 +58,7 @@ prepareData = function(dataset.list, k.decay = 10, k.rare = 5, k.snn = 100, alph
     names(kernel.list)[ds] <- names(dataset.list)[ds]
     
     # Compute SNN
-    snn.list[[ds]] <- Seurat::FindNeighbors(data, verbose = FALSE, compute.SNN = TRUE, k.param = k.snn, prune.SNN = prune)
+    snn.list[[ds]] <- Seurat::FindNeighbors(data, verbose = FALSE, compute.SNN = TRUE, k.param = k.snn, prune.SNN = prune)$snn
     names(snn.list)[ds] <- names(dataset.list)[ds]
   }
   
